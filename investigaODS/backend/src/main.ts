902e9eda5f5257f4377a4bb3e20bc19739670637
Oscar Coyla <ocoyladev@gmail.com>
2025-10-14 18:22:27 -0500

    fix: ensure cookie parser loads with commonjs

--- investigaODS/backend/src/main.ts
+++ investigaODS/backend/src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
